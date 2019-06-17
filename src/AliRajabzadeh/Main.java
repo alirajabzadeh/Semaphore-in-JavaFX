f7acbdf9379b4afbf22c00089f59cce55b9c2e57
Ali Rajabzadeh <alirajabzadeh.devacc@gmail.com>
2019-06-17 08:41:53 +0430

    final commit

--- src/AliRajabzadeh/Main.java
+++ src/AliRajabzadeh/Main.java
@@ -1,21 +1,25 @@
 package AliRajabzadeh;
 
+import javafx.animation.PathTransition;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Pane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 public class Main extends Application {
 
@@ -39,6 +43,15 @@ public class Main extends Application {
         rectangle.setStrokeWidth(3.5);
         root.getChildren().add(rectangle);
     }
+
+    private static Semaphore sem = new Semaphore(4);
+    private static Semaphore sem1 = new Semaphore(2);
+    private static Semaphore sem2 = new Semaphore(1);
+
+    private static double place1 = prefHeight / 2 - 100;
+    private static double place2 = prefHeight / 2 - 35;
+    private static double place3 = prefHeight / 2 + 35;
+    private static double place4 = prefHeight / 2 + 100;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -86,7 +99,54 @@ public class Main extends Application {
         }
 
 
-
+        btn.setOnAction(event -> {
+            for (Circle circle: circles) {
+                new Thread(new Task<Void>() {
+                    Line line = null;
+                    Line line1 = null;
+                    Semaphore thisFinish = new Semaphore(0);
+                    int index = 0;
+                    @Override
+                    protected Void call() throws Exception {
+                        sem.acquire();
+                        sem2.acquire();
+                        if(flag[0]){
+                            line = new Line(circle.getCenterX(), circle.getCenterY(), prefWidth / 2, place1);
+                            line1 = new Line(prefWidth / 2, place1, prefWidth / 2 + 700, prefHeight / 2);
+                            index = 0;
+                            flag[index] = false;
+                        } else if (flag[1]){
+                            line = new Line(circle.getCenterX(), circle.getCenterY(), prefWidth / 2, place2);
+                            line1 = new Line(prefWidth / 2, place2, prefWidth / 2 + 700, prefHeight / 2);
+                            index = 1;
+                            flag[index] = false;
+                        } else if (flag[2]){
+                            line = new Line(circle.getCenterX(), circle.getCenterY(), prefWidth / 2, place3);
+                            line1 = new Line(prefWidth / 2, place3, prefWidth / 2 + 700, prefHeight / 2);
+                            index = 2;
+                            flag[index] = false;
+                        } else if (flag[3]){
+                            line = new Line(circle.getCenterX(), circle.getCenterY(), prefWidth / 2, place4);
+                            line1 = new Line(prefWidth / 2, place4, prefWidth / 2 + 700, prefHeight / 2);
+                            index = 3;
+                            flag[index] = false;
+                        }
+                        PathTransition pt = new PathTransition(Duration.seconds(3), line, circle);
+                        PathTransition pt1 = new PathTransition(Duration.seconds(3), line1, circle);
+                        sem2.release();
+                        pt.setOnFinished(event1 -> thisFinish.release());
+                        Platform.runLater(pt::play);
+                        thisFinish.acquire();
+                        sem1.acquire();
+                        pt1.setOnFinished(event1 -> sem1.release());
+                        Platform.runLater(pt1::play);
+                        flag[index] = true;
+                        sem.release();
+                        return null;
+                    }
+                }).start();
+            }
+        });
 
 
         primaryStage.setTitle("project of Operating System course");
