b9e01c573f56c7a0cbff84462ca3fa1478f4842c
Ali Rajabzadeh <alirajabzadeh.devacc@gmail.com>
2019-06-17 08:15:00 +0430

    Main class created

---
+++ src/AliRajabzadeh/Main.java
@@ -0,0 +1,11 @@
+package AliRajabzadeh;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
